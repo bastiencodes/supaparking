147c67eec8d8cf113edcdfaadeb8599e1d58172c
Bastien <bastienm95@gmail.com>
2021-10-10 19:31:10 +0200

    Show profile email and created_at

--- src/Account.js
+++ src/Account.js
@@ -1,12 +1,11 @@
 import { useState, useEffect } from "react";
 import { supabase } from "./supabaseClient";
-import Avatar from "./Avatar";
+import { Button, Card, Heading } from "evergreen-ui";
 
 export default function Account({ session }) {
   const [loading, setLoading] = useState(true);
-  const [username, setUsername] = useState(null);
-  const [website, setWebsite] = useState(null);
-  const [avatar_url, setAvatarUrl] = useState(null);
+  const [email, setEmail] = useState(null);
+  const [created_at, setCreatedAt] = useState(null);
 
   useEffect(() => {
     getProfile();
@@ -15,11 +14,14 @@ export default function Account({ session }) {
   async function getProfile() {
     try {
       setLoading(true);
+
       const user = supabase.auth.user();
+
+      setEmail(user.email);
 
       let { data, error, status } = await supabase
         .from("profiles")
-        .select(`username, website, avatar_url`)
+        .select(`*`)
         .eq("id", user.id)
         .single();
 
@@ -28,36 +30,7 @@ export default function Account({ session }) {
       }
 
       if (data) {
-        setUsername(data.username);
-        setWebsite(data.website);
-        setAvatarUrl(data.avatar_url);
-      }
-    } catch (error) {
-      alert(error.message);
-    } finally {
-      setLoading(false);
-    }
-  }
-
-  async function updateProfile({ username, website, avatar_url }) {
-    try {
-      setLoading(true);
-      const user = supabase.auth.user();
-
-      const updates = {
-        id: user.id,
-        username,
-        website,
-        avatar_url,
-        updated_at: new Date(),
-      };
-
-      let { error } = await supabase.from("profiles").upsert(updates, {
-        returning: "minimal", // Don't return the value after inserting
-      });
-
-      if (error) {
-        throw error;
+        setCreatedAt(data.created_at);
       }
     } catch (error) {
       alert(error.message);
@@ -67,56 +40,9 @@ export default function Account({ session }) {
   }
 
   return (
-    <div className="form-widget">
-      <Avatar
-        url={avatar_url}
-        size={150}
-        onUpload={(url) => {
-          setAvatarUrl(url);
-          updateProfile({ username, website, avatar_url: url });
-        }}
-      />
-      <div>
-        <label htmlFor="email">Email</label>
-        <input id="email" type="text" value={session.user.email} disabled />
-      </div>
-      <div>
-        <label htmlFor="username">Name</label>
-        <input
-          id="username"
-          type="text"
-          value={username || ""}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="website">Website</label>
-        <input
-          id="website"
-          type="website"
-          value={website || ""}
-          onChange={(e) => setWebsite(e.target.value)}
-        />
-      </div>
-
-      <div>
-        <button
-          className="button block primary"
-          onClick={() => updateProfile({ username, website, avatar_url })}
-          disabled={loading}
-        >
-          {loading ? "Loading ..." : "Update"}
-        </button>
-      </div>
-
-      <div>
-        <button
-          className="button block"
-          onClick={() => supabase.auth.signOut()}
-        >
-          Sign Out
-        </button>
-      </div>
-    </div>
+    <Card>
+      <Heading>Email: {email}</Heading>
+      <Heading>Created at: {created_at}</Heading>
+    </Card>
   );
 }
