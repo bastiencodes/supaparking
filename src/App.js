8d31c0189f74e2cb230d246e7633ed4c85c88363
Bastien <bastienm95@gmail.com>
2021-10-10 17:31:44 +0200

    Check if user exists to show SignUp

--- src/App.js
+++ src/App.js
@@ -4,22 +4,56 @@ import { supabase } from "./supabaseClient";
 import Auth from "./Auth";
 import Account from "./Account";
 import Nav from "./Nav";
+import SignUp from "./SignUp";
+
+const getUser = async () => {
+  const user = supabase.auth.user();
+  let { data, error, status } = await supabase
+    .from("profiles")
+    .select(`id`)
+    .eq("id", user.id)
+    .single();
+  // console.log(data, error, status);
+  return { data, error, status };
+};
 
 export default function Home() {
   const [session, setSession] = useState(null);
 
   useEffect(() => {
-    setSession(supabase.auth.session());
+    async function getUserAndSetSession() {
+      getUser().then(({ data, error, status }) => {
+        const isCreated = data && !error && status === 200;
+        console.log(
+          isCreated ? "User exists in DB." : "User does not exist in DB."
+        );
+        const session = {
+          ...supabase.auth.session(),
+          isCreated,
+        };
+        setSession(session);
+      });
+    }
 
-    supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
+    getUserAndSetSession();
+
+    supabase.auth.onAuthStateChange(async (_event, session) => {
+      getUserAndSetSession();
     });
   }, []);
 
   return (
     <>
       <Nav session={session} />
-      <main>{!session ? <Auth /> : <Account session={session} />}</main>
+      <main>
+        {!session ? (
+          <Auth />
+        ) : session.isCreated ? (
+          <Account session={session} />
+        ) : (
+          <SignUp />
+        )}
+      </main>
     </>
   );
 }
